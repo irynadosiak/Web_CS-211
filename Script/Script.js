fc5fd07b11625b3fd61a194d35add9b7f49ed886
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2020-04-13 19:53:58 +0300

    Update Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -26,7 +26,7 @@ function topFunction() {
 }
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
-    var x = document.getElementById("navDemo");
+    var x = document.getElementById("nav");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
