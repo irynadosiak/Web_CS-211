088956fa19c47f55f2fe917facc5c16f37314557
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2020-06-11 17:50:25 +0300

    Update login.spec.js

--- Lab5/login.spec.js
+++ Lab5/login.spec.js
@@ -1,17 +1,14 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex'
 import VueRouter from "vue-router";
-import Login from "../../src/components/Login";
-
+import login from "../../src/components/login";
 let wrapper;
-describe('Login test', () => {
+describe('login test', () => {
     let actions
     let store
-
     beforeEach(() => {
         const localVue = createLocalVue()
         const router = new VueRouter()
-
         localVue.use(Vuex)
         localVue.use(VueRouter)
         actions = {
@@ -20,20 +17,17 @@ describe('Login test', () => {
         store = new Vuex.Store({
             actions
         })
-        wrapper = shallowMount(Login, {
+        wrapper = shallowMount(login, {
             store,
             localVue,
             router,
             stubs: ['router-link']
         })
     })
-
-
     it('Check for empty field error message', () => {
         wrapper.find('.login').trigger('click')
-        expect(wrapper.vm.$data.error_message).toBe('Please, fill both fields!')
+        expect(wrapper.vm.$data.error_message).toBe('Fill all fields!')
     })
-
     it('Check for login call', () => {
         wrapper.setData({username: 'test', password: 'test'})
         wrapper.find('.login').trigger('click')
